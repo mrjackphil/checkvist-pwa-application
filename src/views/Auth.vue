4fe55827ab16cd7e93e1c1e69b3366a65f5f943d
DegtyarEO <geimerus@gmail.com>
2019-01-24 15:48:59 +0300

    New start view

---
+++ src/views/Auth.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div name="title">Checkvist</div>
+    <div class="desc desc_hide" name="description">Invalid email/password combination</div>
+    <input type="email" name="login" placeholder="Your checkvist email">
+    <input type="password" name="password" placeholder="Your checkvist password">
+    <button name="login" @click="login"></button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Auth extends Vue {
+  private login() {
+    const a = document.querySelector(".desc");
+    a ? a.classList.remove("desc_hide") : null;
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.desc_hide {
+  display: none;
+}
+</style>
+
