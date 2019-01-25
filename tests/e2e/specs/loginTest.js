d36e7868e0ff48af45ecc4dab5e63efa0b0f2928
DegtyarEO <geimerus@gmail.com>
2019-01-25 17:56:55 +0300

    Test for task creation

--- tests/e2e/specs/loginTest.js
+++ tests/e2e/specs/loginTest.js
@@ -9,13 +9,22 @@ module.exports = {
 	  .setValue('input[type=email]', 'login')
 	  .setValue('input[type=password]', 'checkvist_pass')
 	  .click('button');
-    browser.expect.element('[name=title]').text.to.equal('List').before(1000);
-    browser.end();
+	browser.expect.element('[name=title]').text.to.equal('List').before(1000);
   },
-
-  'Is there tasks': browser => {
-    browser.url(process.env.VUE_DEV_SERVER_URL + '/#/list')
-    .waitForElementVisible('[name=task]')
-    .end();
+  'Create a task': browser => {
+	const curtask = Math.random();
+	browser
+	  .url(process.env.VUE_DEV_SERVER_URL+'/#/List')
+	  .waitForElementVisible('button[name=new]')
+	  .click('button[name=new]')
+	  .setValue('input[name=task]', 'New task example #' + curtask)
+	  .click('[name=title]'); //Click anywhere outside of input
+	browser.expect.element('div[text()="New task example #' + curtask).to.be.visible;
+  },
+  'Check if can find task': browser => {
+    browser
+	  .url(process.env.VUE_DEV_SERVER_URL+'/#/List')
+	  .waitForElementVisible('[name=task]', 500)
+	  .end();
   }
 };
