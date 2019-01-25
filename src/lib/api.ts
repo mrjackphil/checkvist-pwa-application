41c7ac0992b5afd6f6c07db1517e8aee41d84669
DegtyarEO <geimerus@gmail.com>
2019-01-25 17:55:17 +0300

    New api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
-import IFetcher from "@/entities/fetch";
+import IFetcher from "@/entities/api";
 import axios from "axios";
 import { jsonToFormData } from "./data";
-import { Order } from "@/entities/datatypes";
+import { Order, ChecklistOptions } from "@/entities/datatypes";
 
 export default class Fetcher implements IFetcher {
   basePath: string;
@@ -16,18 +16,19 @@ export default class Fetcher implements IFetcher {
       "application/x-www-form-urlencoded";
   }
 
-  private async authPost(u: string, opt: any) {
-    const promise = await axios.post(u, jsonToFormData(opt));
+  private async authPost(url: string, opt: any) {
+    const promise = await axios.post(url, jsonToFormData(opt));
     return promise.data;
   }
 
-  private async get(u: string) {
-    const token = localStorage.getItem("token");
+  private async get(url: string, o?: any) {
+	const token = localStorage.getItem("token");
     const promise = await axios({
       method: "get",
-      url: u,
+      url: url,
       params: {
-        token: token
+		token: token,
+		...o
       }
     });
     return promise.data;
@@ -61,7 +62,7 @@ export default class Fetcher implements IFetcher {
     });
   }
 
-  async checklists(arch?: boolean, ord?: Order, skip?: boolean) {
-    return this.get("/checklists.json");
+  async checklists(opt?: ChecklistOptions) {
+    return this.get("/checklists.json", opt);
   }
 }
