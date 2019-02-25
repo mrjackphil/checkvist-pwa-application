d31019e08f36632056aaa31369987d47511428cf
DegtyarEO <geimerus@gmail.com>
2019-02-25 16:59:28 +0300

    Add swipe task component

---
+++ src/views/components/SwipeTask.vue
@@ -0,0 +1,67 @@
+<template>
+  <swipe-list :items="items" transition-key="id">
+    <!--
+    @swipeout:contentclick="contentClick"
+    @swipeout:click="itemClick"
+    @swipeout:doubleclick="itemDblClick"
+    -->
+    <template slot-scope="{ item }">
+      <!-- item is the corresponding object from the array -->
+      <!-- index is clearly the index -->
+      <!-- revealLeft is method which toggles the left side -->
+      <!-- revealRight is method which toggles the right side -->
+      <!-- close is method which closes an opened side -->
+      <div class="card-content">
+        <!-- style content how ever you like -->
+        <h2>{{ item.content }}</h2>
+      </div>
+    </template>
+    <template slot="left">
+      <!-- remove <template slot="left" slot-scope="{ item }"> if you dont wanna have left swipe side  -->
+      <!-- close is method which closes an opened side -->
+      <div class="swipeout-action">
+        <i class="fa fa-cloud"></i>
+      </div>
+      <div class="swipeout-action">
+        <!-- place icon here or what ever you want -->
+      </div>
+    </template>
+    <template slot="right" slot-scope="{ item }">
+      <!-- remove <template slot="right" slot-scope="{ item }"> if you dont wanna have right swipe side  -->
+      <!-- close is method which closes an opened side -->
+      <remove-list :listId="item.id"></remove-list>
+    </template>
+    <div slot="empty">
+      <!-- change mockSwipeList to an empty array to see this slot in action  -->
+      list is empty ( filtered or just empty )
+    </div>
+  </swipe-list>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+// @ts-ignore
+import { SwipeList, SwipeOut } from "vue-swipe-actions";
+import RemoveList from "./BtnRemoveList.vue";
+import { Task } from "@/entities/datatypes";
+
+@Component({ components: { SwipeOut, SwipeList, RemoveList } })
+export default class SwipeActionList extends Vue {
+  @Prop() private items: Task[] = this.items;
+  private name = "swipe-action-list";
+}
+</script>
+
+<style lang="scss">
+.swipeout-action {
+  width: 5rem;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.swipeout-red-back {
+  background: red;
+  color: white;
+}
+</style>
+
